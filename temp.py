5b617c974a582b5138bbe80f1c8f31592c5365bc
HoloZhou <97168950+HoloZhou@users.noreply.github.com>
2022-07-31 13:00:45 +0800

    Update temp.py

--- temp.py
+++ temp.py
@@ -11,23 +11,23 @@ import pandas as pd
 import numpy as np
 import openpyxl
 #%%
-age=st.number_input('年龄')
-se=st.selectbox('性别',options=('男','女'))
-if se=='男':
-    sex=1
-else:
-    sex=0
-cp=st.selectbox('心律',options=(0,1,2,3))
-trestbps=st.number_input('血压')
-chol=st.number_input('晨起血糖')
-fbs=st.selectbox('餐后2小时血糖',options=(0,1))
-restecg=st.selectbox('手术史',options=(0,1))
-thalach=st.number_input('Insert thalach')
-exang=st.selectbox('exang',options=(0,1))
-oldpeak=st.number_input('Insert oldpeak')
-slope=st.selectbox('slope',options=(0,1,2))
-ca=st.selectbox('ca',options=(0,1))
-thal=st.selectbox('thal',options=(1,2,3))
+age=str(st.number_input('年龄'))
+se=(st.selectbox('性别',options=('男','女'))
+    if se =='男':
+        sex=str(1)
+    else:
+        sex=str(0)
+cp=str(st.selectbox('心律',options=(0,1,2,3)))
+trestbps=str(st.number_input('血压'))
+chol=str(st.number_input('晨起血糖'))
+fbs=str(st.selectbox('餐后2小时血糖',options=(0,1)))
+restecg=str(st.selectbox('手术史',options=(0,1)))
+thalach=str(st.number_input('Insert thalach'))
+exang=str(st.selectbox('exang',options=(0,1)))
+oldpeak=str(st.number_input('Insert oldpeak'))
+slope=str(st.selectbox('slope',options=(0,1,2)))
+ca=str(st.selectbox('ca',options=(0,1)))
+thal=str(st.selectbox('thal',options=(1,2,3)))
 allfactor=[age,sex, cp,trestbps,chol,fbs,restecg,thalach,
         exang,oldpeak,slope,ca,thal]
 #allfactor=pd.DataFrame(allfactor,columns=['age','sex', 'cp','trestbps','chol','fbs','restecg','thalach',
